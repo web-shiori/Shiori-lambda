81b9c5da59cf8a0905658e19cc355bacae74681d
masatora <atarashi.masatora@gmail.com>
2021-10-01 20:54:40 +0900

    S3のオブジェクトを取得

--- handler.go
+++ handler.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func excuteFunction()  {
-	fmt.Println("Hello, Lambda")
+func s3Handler(ctx context.Context, event events.S3Event) (err error) {
+	for _, record := range event.Records {
+		bucket := record.S3.Bucket.Name
+		key := record.S3.Object.Key
+		fmt.Printf("bucket: %s, key: %s", bucket, key)
+	}
+	return err
 }
 
 func main() {
-	lambda.Start(excuteFunction)
+	lambda.Start(s3Handler)
 }
