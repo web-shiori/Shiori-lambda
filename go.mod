28bff833dddba094381cab9302d665dc179e7398
masatora <atarashi.masatora@gmail.com>
2021-10-01 22:17:56 +0900

    Amazon Textractでローカルの画像からテキストを抽出する

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/MasatoraAtarashi/Shiori-lambda
 
 go 1.16
 
-require github.com/aws/aws-lambda-go v1.27.0
+require (
+	github.com/aws/aws-lambda-go v1.27.0
+	github.com/aws/aws-sdk-go v1.40.53
+)
--- handler.go
+++ handler.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/textract"
+	"io/ioutil"
 )
 
 func s3Handler(ctx context.Context, event events.S3Event) {
@@ -17,6 +20,34 @@ func s3Handler(ctx context.Context, event events.S3Event) {
 	fmt.Println("Lambda Finish")
 }
 
+var textractSession *textract.Textract
+
 func main() {
-	lambda.Start(s3Handler)
+	//lambda.Start(s3Handler)
+
+	// TODO: S3のファイルを参照するようにする
+	file, err := ioutil.ReadFile("sample.png")
+	if err != nil {
+		panic(err)
+	}
+
+	// TODO: refactoring
+	textractSession = textract.New(session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	})))
+
+	resp, err := textractSession.DetectDocumentText(&textract.DetectDocumentTextInput{
+		Document: &textract.Document{
+			Bytes: file,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, w := range resp.Blocks {
+		if *w.BlockType == "WORD" {
+			fmt.Println(*w.Text)
+		}
+	}
 }
