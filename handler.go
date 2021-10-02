1a6863f090686b7ce425526ce7654719a9f14823
masatora <atarashi.masatora@gmail.com>
2021-10-02 18:57:54 +0900

    PDFページ数を取得するロジックを実装

--- handler.go
+++ handler.go
@@ -16,11 +16,11 @@ func s3Handler(ctx context.Context, event events.S3Event) {
 		bucket := record.S3.Bucket.Name
 		key := record.S3.Object.Key
 		fmt.Printf("bucket: %s, key: %s\n", bucket, key)
-		pageNumber, err := detectPageNumber(bucket, key)
+		pageNum, err := detectPageNumber(bucket, key)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(pageNumber)
+		fmt.Printf("pageNum: %d", pageNum)
 	}
 	fmt.Println("Lambda Finish")
 }
@@ -28,13 +28,13 @@ func s3Handler(ctx context.Context, event events.S3Event) {
 // PDFのページ数を取得する
 func detectPageNumber(bucket string, key string) (pageNum int, err error) {
 	var textractSession *textract.Textract
+	var simplePageNumExtracter SimplePageNumExtracter
 
 	// S3のファイルをTextractにかける
 	textractSession = textract.New(session.Must(session.NewSession(&aws.Config{
 		// TODO: 環境変数から取得する
 		Region: aws.String("us-east-1"),
 	})))
-
 	resp, err := textractSession.DetectDocumentText(&textract.DetectDocumentTextInput{
 		Document: &textract.Document{
 			S3Object: &textract.S3Object{
@@ -47,72 +47,22 @@ func detectPageNumber(bucket string, key string) (pageNum int, err error) {
 		return 0, err
 	}
 
-
-	// PDFのページ数を抽出する
-	// TODO: impl
-	/*
-	レスポンスの例
-	AIDataTechnologyMap_210520.po
-	6
-	/
-	108
-	I
-	-
-	100%
-	+
-	I
-	:
-	a
-	am
-	4
-	Anaheim
-	44
-	Corona
-	46
-	Zumwalt
-	48
-	Orion
-	Annotator
-	50
-	Kafon
-	52
-	5
-	Phalanx
-	54
-	T
-	56
-	CyberZ
-	ACTech
-	58
-	CAM
-	Fensi
-	60
-	XT17/ADT
-	ABEMAOTA
-	62
-	64
-	6
-	Engineering
-	How
-	We
-	Work
-	70
-	70
-	7b
-	7
-	XT17/ADT
-	82
-	*/
+	// Textractで検知した文字列をスライスにする
+	var detectWordSlice []string
 	for _, w := range resp.Blocks {
 		if *w.BlockType == "WORD" {
-			fmt.Println(*w.Text)
+			detectWordSlice = append(detectWordSlice, *w.Text)
 		}
 	}
 
-	// PDFのページ数を返す
-	return 0, err
-}
+	// PDFのページ数を抽出する
+	pageNum, err = simplePageNumExtracter.extractPageNum(detectWordSlice)
+	if err != nil {
+		return 0, err
+	}
 
+	return
+}
 
 func main() {
 	lambda.Start(s3Handler)
